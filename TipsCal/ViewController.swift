8e733a28b63e7da8276aaf29dbf672790554c209
kev-th <kevinkthaw@gmail.com>
2019-01-10 22:38:59 -0500

    Initial Commit

---
+++ TipsCal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipsCal
+//
+//  Created by Kevin Thaw on 1/10/19.
+//  Copyright © 2019 Kevin Thaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
